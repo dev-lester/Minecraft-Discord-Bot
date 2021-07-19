83900cc8f581b6a603661a352e70e752d3c1f919
dev-lester <lesters821@gmail.com>
2021-07-19 18:00:40 +0800

    minor changes

--- server.js
+++ server.js
@@ -2,47 +2,44 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
+const { prefix } = require('./config.json');
+const token = process.env.DISCORD_TOKEN;
 const client = new Discord.Client();
+
+const ping = require('minecraft-server-util');
 
 client.on('ready', () => {
 	console.log('I am running!');
 });
 
-// client.on('message', message => {
-// 	if (message.content === `${prefix}ping`) {
-// 		// send back "Pong." to the channel the message was sent in
-// 		message.channel.send('Pong!');
-// 	}
-// 	else if (message.content === `${prefix}bark`) {
-// 		message.channel.send('Continental Pakyu!');
-// 	}
-// 	else if (message.content === `${prefix}server`) {
-// 		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-// 	}
-// 	else if (message.content === `${prefix}info-user`) {
-// 		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
-// 	}
-// });
+client.on('message', message => {
+	const args = message.content.substring(prefix.length).split(/ +/);
+	const ipAdress = process.env.MINECRAFT_IP_ADRESS;
 
-client.on('message', message =>{
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
+	switch(args[0]) {
+	case 'mc':
+		ping.status(ipAdress)
+			.then((response) => {
+				const Embed = new Discord.MessageEmbed()
+					.setTitle('Server Status')
+					.addField('Description', response.description)
+					.addField('Server IP', 'Please ask permission to boss lestaH!')
+					.addField('Port', response.port)
+					.addField('Server Version', response.version)
+					.addField('Online Players', response.onlinePlayers)
+					.addField('Max Players', response.maxPlayers);
 
-	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const command = args.shift().toLocaleLowerCase();
-	// ...
-	// Using the new `command` variable, this makes it easier to manage!
-	// You can switch your other commands to this format as well
-	if (command === '!bark') {
-		message.channel.send('Continental Pakyu!');
+				message.channel.send(Embed);
+
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+		break;
+	case 'malas':
+		message.channel.send('RAINIEL APOSTOL VILLANUEVA');
+		break;
 	}
-	else if (command === 'args-info') {
-		if (!args.length) {
-			return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-		}
-		message.channel.send(`Command name: ${command}\nArguments: ${args}`);
-	}
-
 });
 
 client.login(token);

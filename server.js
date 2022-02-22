1b3af81c411c22c6201688efc13371eb2326d1bc
dev-lester <lesters821@gmail.com>
2022-02-23 03:53:49 +0800

    minor fix

--- server.js
+++ server.js
@@ -21,21 +21,40 @@ client.on('message', message => {
 		ping.status(ipAdress)
 			.then((response) => {
 				const Embed = new Discord.MessageEmbed()
-					.setTitle('Server Status')
-					.addField('Description', response.description)
-					.addField('Server IP', 'Please ask permission to boss lestaH!')
+					.setTitle('SPIELER NABE Minecraft server status')
+					.addField('Description', 'Welcome to SPIELER NABE smp mga bobo')
+					.addField('Server IP', 'wag mahihiyang mag tanong kay Lester!')
 					.addField('Port', response.port)
 					.addField('Server Version', response.version)
 					.addField('Online Players', response.onlinePlayers)
-					.addField('Max Players', response.maxPlayers);
-
+					.addField('Max Players', response.maxPlayers)
 				message.channel.send(Embed);
 
+				console.error({response});
 			})
 			.catch((error) => {
 				console.error(error);
 			});
 		break;
+
+	case 'mc-homies':
+		ping.status(ipAdress)
+		.then((response) => {
+			const Embed = new Discord.MessageEmbed()
+				.setTitle('SPIELER NABE Minecraft members')
+				.addField('Members', response.rawResponse.players.sample.map.name)
+			message.channel.send(Embed);
+			console.log(response.rawResponse.players);
+
+		})
+		.catch((error) => {
+			const Embed = new Discord.MessageEmbed()
+				.addField(error)
+			message.channel.send(Embed);
+			console.error(error);
+		});
+		break;
+		
 	case 'malas':
 		message.channel.send('RAINIEL APOSTOL VILLANUEVA');
 		break;
